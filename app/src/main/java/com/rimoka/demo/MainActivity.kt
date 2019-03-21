f4a59b4e01d9c1c149c9c483216ec994177b50ec
MBA0135s <mba0135s@MBA0135s.local>
2019-03-21 16:14:13 +0700

    a

--- app/src/main/java/com/rimoka/demo/MainActivity.kt
+++ app/src/main/java/com/rimoka/demo/MainActivity.kt
@@ -3,15 +3,12 @@ package com.rimoka.demo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log.i
-import com.crashlytics.android.Crashlytics
-import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Fabric.with(this, Crashlytics())
     }
 
     /**
